a00ef0d7d3f8952b18d596095502d513712c1ae5
sergey <sierghieigavrilov053@gmail.com>
2020-12-11 18:05:52 +0200

    compare_if_cycles 4

--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -3,16 +3,22 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleInfo {
 
     public static boolean isTriangle(int a, int b, int c) {
-        if (a < c + b) return true;
-        if (c < a + b) return true;
-        return b < c + a;
+        if (a >= c + b) return false;
+        if (c >= a + b) return false;
+        return b >= c + a;
     }
 
     public static boolean isRightTriangle(int a, int b, int c) {
-        return a * a + b * b == c * c;
+        return b * b + c * c == a * a;
+
     }
 
     public static boolean isIsoscelesTriangle(int a, int b, int c) {
         return a == b || a == c || b == c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isTriangle(4, 1, 5));
+        System.out.println(isRightTriangle(5, 4, 3));
     }
 }
--- src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleSimpleInfo.java
@@ -3,18 +3,23 @@ package ru.progwards.java1.lessons.compare_if_cycles;
 public class TriangleSimpleInfo {
 
     public static int maxSide(int a, int b, int c) {
-        if (a > b && a > c) return a;
-        if (b > a && b > c) return b;
+        if (a >= b && a >= c) return a;
+        if (b >= a && b >= c) return b;
         return c;
     }
 
     public static int minSide(int a, int b, int c) {
-        if (a < b && a < c) return a;
-        if (b < a && b < c) return b;
+        if (a <= b && a <= c) return a;
+        if (b <= a && b <= c) return b;
         return c;
     }
 
     public static boolean isEquilateralTriangle(int a, int b, int c) {
         return a == b && a == c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSide(10, 10, 8));
+        System.out.println(minSide(3, 3, 4));
     }
 }
