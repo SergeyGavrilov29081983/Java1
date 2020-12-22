372ce63678e82c740c8e519e232266020efb89fa
sergey <sierghieigavrilov053@gmail.com>
2020-12-22 20:10:53 +0200

    classes3

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -30,7 +30,7 @@ public class Animal {
 
     @Override
     public String toString() {
-        return "I am " + getKind() + "," + "eat " + getFoodKind();
+        return "I am " + getKind() + ", " + "eat " + getFoodKind();
     }
 
     public String toStringFull() {
