c6ba6e7ae4d0f3eeb9b65a97d8a4a2681a4bf9db
sergey <sierghieigavrilov053@gmail.com>
2020-12-22 21:49:13 +0200

    classes4

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -10,39 +10,32 @@ public class ComplexNum {
         this.b = b;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new ComplexNum(1, 56).toString());
+    }
+
     @Override
     public String toString() {
         return a + "+" + b + "i";
     }
 
     public ComplexNum add(ComplexNum num) {
-        a = this.a + num.a;
-        b = this.b + num.b;
-        return new ComplexNum(a, b);
-
-       // (a + bi) + (c + di) = (a + c) + (b + d)i;
+        return new ComplexNum(this.a + num.a, this.b + num.b);
     }
 
-
     public ComplexNum sub(ComplexNum num) {
-
-        //(a + bi) - (c + di) = (a - c) + (b - d) i
-        return null;
+        return new ComplexNum(this.a - num.a, this.b - num.b);
     }
 
     public ComplexNum mul(ComplexNum num) {
 
-       // (a + bi) * (c + di) = (a * c - b * d) + (b * c + a * d) i
-        return null;
+        // (a + bi) * (c + di) = (a * c - b * d) + (b * c + a * d) i
+        return new ComplexNum(this.a * num.a, this.b * b);
     }
 
     public ComplexNum div(ComplexNum num) {
 
-       // (a + bi) / (c + di) = (a * c + b * d) / (c * c + d * d) + ((b * c - a * d) / (c * c + d * d)) i
+        // (a + bi) / (c + di) = (a * c + b * d) / (c * c + d * d) + ((b * c - a * d) / (c * c + d * d)) i
         return null;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new ComplexNum(1,56).toString());
     }
 }
