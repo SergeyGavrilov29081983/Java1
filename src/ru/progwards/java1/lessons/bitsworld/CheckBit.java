6df8817ce1b51fe013d4f803bb201430c1cfe125
sergey <sierghieigavrilov053@gmail.com>
2021-01-08 19:23:23 +0200

    bits4

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -3,6 +3,10 @@ package ru.progwards.java1.lessons.bitsworld;
 public class CheckBit {
 
     public static int checkBit(byte value, int bitNumber) {
-        return (value >>> (7 - bitNumber)) & 1;
+        return (value >> (7 - bitNumber)) & 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkBit((byte) 76, 0));
     }
 }
