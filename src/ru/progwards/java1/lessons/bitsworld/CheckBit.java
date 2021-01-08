61b51df3c2936a369f752eb4a253f035318c2317
sergey <sierghieigavrilov053@gmail.com>
2021-01-08 17:18:39 +0200

    bits2

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -1,4 +1,8 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
+
+    public static int checkBit(byte value, int bitNumber) {
+        return ((value >> (bitNumber-1)) & 1);
+    }
 }
