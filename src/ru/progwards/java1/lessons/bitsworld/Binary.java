ee2e3e1cd43ac35803b25239fe15c76e7ad7bec9
sergey <sierghieigavrilov053@gmail.com>
2021-01-09 16:05:36 +0200

    bits7

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -1,4 +1,21 @@
 package ru.progwards.java1.lessons.bitsworld;
 
 public class Binary {
+
+    private byte num;
+
+    public Binary(byte num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for(int i = 7; i >= 0 ; i--) {
+            int mask = 1 << i;
+            result.append((num & mask) != 0 ? "1" : "0");
+        }
+        result.replace(result.length() - 1, result.length(), "");
+        return result.toString();
+    }
 }
