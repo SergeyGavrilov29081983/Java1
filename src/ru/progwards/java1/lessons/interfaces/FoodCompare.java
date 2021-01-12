d73355383a0ad446ca8491b20af948db3b91a328
sergey <sierghieigavrilov053@gmail.com>
2021-01-12 18:53:41 +0200

    interfaces-4

--- src/ru/progwards/java1/lessons/interfaces/FoodCompare.java
+++ src/ru/progwards/java1/lessons/interfaces/FoodCompare.java
@@ -1,7 +1,5 @@
 package ru.progwards.java1.lessons.interfaces;
 
-import ru.progwards.java1.lessons.classes.Animal;
-
 public interface FoodCompare {
 
     public int compareFoodPrice(Animal animal);
