3c4c900e803c3a2b51ba1bd21fbd5d52190d38fa
sergey <sierghieigavrilov053@gmail.com>
2021-01-13 12:58:49 +0200

    interfaces-6

---
+++ src/ru/progwards/java1/lessons/interfaces/Animal.java
@@ -0,0 +1,83 @@
+package ru.progwards.java1.lessons.interfaces;
+
+
+import java.util.Objects;
+
+
+public class Animal implements FoodCompare {
+
+    private double weight;
+
+    public FoodKind foodKind;
+
+    public Animal(double weight) {
+        this.weight = weight;
+    }
+
+    public AnimalKind getKind() {
+        return AnimalKind.ANIMAL;
+    }
+
+    public FoodKind getFoodKind() {
+        return FoodKind.UNKNOWN;
+    }
+
+    public double getFood1kgPrice() {
+        switch (foodKind) {
+            case HAY:
+                return 20;
+            case CORN:
+                return 50;
+            case UNKNOWN:
+                return 0;
+        }
+        return 0;
+    }
+
+    public double getFoodPrice() {
+        return  calculateFoodWeight() * getFood1kgPrice();
+
+    }
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getFoodCoeff() {
+        return 0.02;
+    }
+
+    public double calculateFoodWeight() {
+        return getWeight() * getFoodCoeff();
+    }
+
+    @Override
+    public String toString() {
+        return "I am " + getKind() + ", " + "eat " + getFoodKind();
+    }
+
+    public String toStringFull() {
+        return toString() + " " + calculateFoodWeight();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Double.compare(animal.weight, weight) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight);
+    }
+
+    @Override
+    public int compareFoodPrice(Animal animal) {
+        return Double.compare(this.getFoodPrice(), animal.getFoodPrice());
+    }
+
+
+}
