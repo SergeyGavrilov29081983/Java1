e1ab587aabea23326605602c10d35ee2fdf4d105
sergey <sierghieigavrilov053@gmail.com>
2021-01-28 17:36:53 +0200

    interfaces-8

--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -4,6 +4,24 @@ public interface CompareWeight {
 
     public CompareResult compareWeight(Animal animal);
 
+    public static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            int min = a[i];
+            int min_i = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < min) {
+                    min = a[j];
+                    min_i = j;
+                }
+            }
+            if (i != min_i) {
+                int tmp = a[i];
+                a[i] = a[min_i];
+                a[min_i] = tmp;
+            }
+        }
+    }
+
     public enum CompareResult {
         LESS, EQUAL, GREATER;
     }
