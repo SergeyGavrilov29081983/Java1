cfc6ad7eb9456eaab269fcb53a2848b257335818
sergey <sierghieigavrilov053@gmail.com>
2021-02-09 09:59:23 +0200

    bigints-1

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -6,22 +6,23 @@ public class AbsInteger {
     public int integer;
 
     public static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        AbsInteger abs = new AbsInteger();
-        if (num1 instanceof ByteInteger || num2 instanceof ByteInteger) {
-            sum = num1.toIntObj().getInteger() + num2.toIntObj().getInteger();
-        }
-        if (num1 instanceof ShortInteger || num2 instanceof ShortInteger) {
-            sum = num1.toIntObj().getInteger() + num2.toIntObj().getInteger();
-        }
         sum = num1.toIntObj().getInteger() + num2.toIntObj().getInteger();
-
-        return abs;
+        if (sum >= -127 && sum <= 128) {
+            return new ByteInteger((byte) sum);
+        }
+        if (sum >= -32768 && sum <= 32767) {
+            return new ShortInteger((short) sum);
+        }
+        return new IntInteger(sum);
     }
+
 
     public static void main(String[] args) {
 
-        add(new ShortInteger((short) 12345), new IntInteger(8));
+        AbsInteger t = add(new ShortInteger((short) 12345), new IntInteger(8));
+        System.out.println(t.getClass());
         System.out.println(sum);
+
     }
 
     public IntInteger toIntObj() {
