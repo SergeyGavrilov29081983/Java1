91ff4b4d976e21c0a497f30cc4d24869c7ca0573
sergey <sierghieigavrilov053@gmail.com>
2021-03-26 19:47:09 +0200

    sets 3

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -3,9 +3,9 @@ package ru.progwards.java1.lessons.sets;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class LettersInFile {
@@ -14,16 +14,8 @@ public class LettersInFile {
         List<String> lines = Files.readAllLines(Paths.get(fileName));
         StringBuilder builder = new StringBuilder();
         for (String str : lines) {
-            builder.append(str.replaceAll("[^a-zA-Zа-яА-Я ]", ""));
+            builder.append(str.replaceAll("[^a-zA-Zа-яА-Я]", ""));
         }
-        Set<Character> sort = new HashSet<>();
-        String result = builder.toString();
-        for (Character character : result.toCharArray()) {
-            sort.add(character);
-        }
-        for (Character character : sort) {
-            builder.append(character);
-        }
-        return builder.toString();
+        return Stream.of(builder.toString().split("")).sorted().collect(Collectors.joining());
     }
 }
