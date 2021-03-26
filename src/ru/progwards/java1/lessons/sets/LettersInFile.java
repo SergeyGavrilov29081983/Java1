ac5ee410575a31f7afc1c94470d069e7b2276509
sergey <sierghieigavrilov053@gmail.com>
2021-03-26 16:17:19 +0200

    sets 3

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -3,9 +3,10 @@ package ru.progwards.java1.lessons.sets;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
+
 
 public class LettersInFile {
 
@@ -15,7 +16,7 @@ public class LettersInFile {
         for (String str : lines) {
             builder.append(str.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ ]", ""));
         }
-        Set<Character> sort = new TreeSet<>();
+        Set<Character> sort = new HashSet<>();
         String result = builder.toString();
         for (Character character : result.toCharArray()) {
             sort.add(character);
