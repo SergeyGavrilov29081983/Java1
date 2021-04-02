f3adaf55a7f3f40a63702ab84f1e9ab2908b7b9b
sergey <sierghieigavrilov053@gmail.com>
2021-04-02 16:41:18 +0200

    queue

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -1,24 +1,56 @@
 package ru.progwards.java1.lessons.queues;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
+import java.util.*;
 
 public class CollectionsSort {
 
     public static void mySort(Collection<Integer> data) {
-
+        List<Integer> result = new ArrayList<>(data);
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = i + 1; j < result.size(); j++) {
+                if (result.get(i) > result.get(j)) {
+                    int a1 = result.get(i);
+                    int a2 = result.get(j);
+                    result.add(i, a1);
+                    result.add(j, a2);
+                }
+            }
+        }
     }
 
     public static void minSort(Collection<Integer> data) {
-        Collection<Integer> result = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(data);
+        List<Integer> result = new ArrayList<>(list.size());
+        int min;
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            min = Collections.min(list);
+            result.add(min);
+            list.remove((Integer) min);
+        }
+        list = new ArrayList<>(data);
+        Collections.copy(list, result);
+
+
+
     }
 
+
     public static void collSort(Collection<Integer> data) {
-        Collections.sort(new ArrayList<>(data));
+        List<Integer> result = new ArrayList<>(data);
+        Collections.sort(result);
     }
 
     public static Collection<String> compareSort() {
-        return null;
+
+       /* в котором сравнить производительность методов и вернуть их имена, отсортированные в порядке производительности,
+                первый - самый быстрый.
+                В случае равенства производительности каких-то методов, возвращать их названия
+        в алфавитном порядке.*/
+    return null;
+    }
+
+    public static void main(String[] args) {
+        CollectionsSort.minSort(Arrays.asList(4, 12, 66, 60, 36, 6, 69, 14));
     }
 }
--- src/ru/progwards/java1/lessons/queues/OrderQueue.java
+++ src/ru/progwards/java1/lessons/queues/OrderQueue.java
@@ -1,10 +1,17 @@
 package ru.progwards.java1.lessons.queues;
 
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class OrderQueue {
-    private PriorityQueue<Order> queue = new PriorityQueue<>();
+    Comparator<Order> comparator = new Comparator<Order>() {
+        @Override
+        public int compare(Order o1, Order o2) {
+            return Integer.compare(o1.getNum(), o2.getNum());
+        }
+    };
+    private PriorityQueue<Order> queue = new PriorityQueue<>(comparator);
 
     public void add(Order order) {
         queue.offer(order);
