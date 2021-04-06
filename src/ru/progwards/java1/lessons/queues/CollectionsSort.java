1859a037ab095e04f566ab5fc9e3156e6e7839b6
sergey <sierghieigavrilov053@gmail.com>
2021-04-06 18:59:37 +0200

    queue 1

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class CollectionsSort {
 
     public static void mySort(Collection<Integer> data) {
-
         List<Integer> list = new ArrayList<>(data);
         for (int i = 0; i < list.size(); i++) {
             for (int j = i + 1; j < list.size(); j++) {
@@ -39,7 +38,6 @@ public class CollectionsSort {
     }
 
     static Collection<String> compareSort() {
-
         long start = System.currentTimeMillis();
         mySort(randomCollection());
         long time = System.currentTimeMillis() - start;
