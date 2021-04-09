d80a5f22cfef6ea1bdba86091a941b987e85f9e1
sergey <sierghieigavrilov053@gmail.com>
2021-04-09 22:17:11 +0200

    maps

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -6,15 +6,23 @@ import java.util.TreeMap;
 
 public class FiboMapCache {
 
-      Map<Integer, BigDecimal> fiboCache;
+     private Map<Integer, BigDecimal> fiboCache;
+     boolean isCacheOn;
 
     public FiboMapCache(boolean cacheOn) {
+        isCacheOn = cacheOn;
         if (cacheOn) {
             fiboCache = new TreeMap<>();
         }
     }
 
+
+
     public BigDecimal fiboNumber(int n) {
+        if (fiboCache.containsKey(n)) {
+            return fiboCache.get(n);
+        }
+
         int first;
         int next = 1;
         int current = 0;
@@ -23,11 +31,36 @@ public class FiboMapCache {
             next = current;
             current = first + next;
         }
+        BigDecimal result = new BigDecimal(n);
+        if (!fiboCache.containsKey(n)) {
+            return fiboCache.put(n, result);
+        }
+
         return new BigDecimal(n);
     }
-
 
     public void clearCahe() {
         fiboCache = null;
     }
+
+    public static void test() {
+        FiboMapCache fiboMapCache = new FiboMapCache(true);
+        long start = System.currentTimeMillis();
+
+        for (int i = 0 ; i <=1000 ; i++) {
+            fiboMapCache.fiboNumber(i);
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("fiboNumber cacheOn=" + fiboMapCache.isCacheOn +  "время выполнения "  + time);
+
+        FiboMapCache fiboMapCache1 = new FiboMapCache(false);
+        long start1 = System.currentTimeMillis();
+
+        for (int i = 0 ; i <=1000 ; i++) {
+            fiboMapCache1.fiboNumber(i);
+        }
+        long time1 = System.currentTimeMillis() - start;
+        System.out.println("fiboNumber cacheOn=" + fiboMapCache1.isCacheOn +  "время выполнения "  + time1);
+    }
 }
+
