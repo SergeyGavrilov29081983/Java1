c991da20471a7033d24769fa302a2515aaba32cd
sergey <sierghieigavrilov053@gmail.com>
2021-05-12 19:21:47 +0200

    maps

--- src/ru/progwards/java1/lessons/datetime/SessionManager.java
+++ src/ru/progwards/java1/lessons/datetime/SessionManager.java
@@ -1,16 +1,13 @@
 package ru.progwards.java1.lessons.datetime;
 
-import java.time.*;
-import java.util.Collection;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class SessionManager {
 
     private Map<String, UserSession> sessions = new HashMap<>();
     private int sessionValid;
-
 
     public SessionManager(int sessionValid) {
         this.sessionValid = sessionValid;
@@ -25,9 +22,9 @@ public class SessionManager {
     }
 
     public UserSession get(int sessionHandle) {
-        for(Map.Entry<String, UserSession> session : sessions.entrySet()) {
+        for (Map.Entry<String, UserSession> session : sessions.entrySet()) {
             UserSession value = session.getValue();
-            if (value.sessionHandle == sessionHandle) {
+            if (value.getSessionHandle() == sessionHandle) {
                 value.setLastAccess(LocalDateTime.now());
                 return value;
             }
@@ -35,64 +32,24 @@ public class SessionManager {
         return null;
     }
 
-            public void delete(int sessionHandle) {
-                for (Map.Entry<String, UserSession> session : sessions.entrySet()) {
-                    UserSession value = session.getValue();
-                    if (value.sessionHandle == sessionHandle) {
-                        sessions.remove(value.getUserName());
+    public void delete(int sessionHandle) {
+        for (Map.Entry<String, UserSession> session : sessions.entrySet()) {
+            UserSession value = session.getValue();
+            if (value.getSessionHandle() == sessionHandle) {
+                sessions.remove(value.getUserName());
 
-                    }
-                }
             }
-
-
-public void deleteExpired() {
-    for (Map.Entry<String, UserSession> session : sessions.entrySet()) {
-        LocalDateTime AccessExp = session.getValue().getLastAccess().plusSeconds(sessionValid);
-        if (AccessExp.isBefore(LocalDateTime.now())) {
-            sessions.remove(session.getKey());
-        }
-    }
-}
-
-
-    static class UserSession {
-        private int sessionHandle;
-        private String userName;
-        private LocalDateTime lastAccess;
-
-        public UserSession(String userName) {
-            Random random = new Random();
-            this.userName = userName;
-            sessionHandle = random.nextInt();
-            lastAccess = LocalDateTime.now();
-        }
-
-        public int getSessionHandle() {
-            return sessionHandle;
-        }
-
-        public void setSessionHandle(int sessionHandle) {
-            this.sessionHandle = sessionHandle;
-        }
-
-        public String getUserName() {
-            return userName;
-        }
-
-        public void setUserName(String userName) {
-            this.userName = userName;
-        }
-
-        public LocalDateTime getLastAccess() {
-            return lastAccess;
-        }
-
-        public void setLastAccess(LocalDateTime lastAccess) {
-            this.lastAccess = lastAccess;
         }
     }
 
+    public void deleteExpired() {
+        for (Map.Entry<String, UserSession> session : sessions.entrySet()) {
+            LocalDateTime AccessExp = session.getValue().getLastAccess().plusSeconds(sessionValid);
+            if (AccessExp.isBefore(LocalDateTime.now())) {
+                sessions.remove(session.getKey());
+            }
+        }
+    }
 }
 
 
---
+++ src/ru/progwards/java1/lessons/datetime/UserSession.java
@@ -0,0 +1,41 @@
+package ru.progwards.java1.lessons.datetime;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class UserSession {
+    private int sessionHandle;
+    private String userName;
+    private LocalDateTime lastAccess;
+
+    public UserSession(String userName) {
+        Random random = new Random();
+        this.userName = userName;
+        sessionHandle = random.nextInt();
+        lastAccess = LocalDateTime.now();
+    }
+
+    public int getSessionHandle() {
+        return sessionHandle;
+    }
+
+    public void setSessionHandle(int sessionHandle) {
+        this.sessionHandle = sessionHandle;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDateTime getLastAccess() {
+        return lastAccess;
+    }
+
+    public void setLastAccess(LocalDateTime lastAccess) {
+        this.lastAccess = lastAccess;
+    }
+}
