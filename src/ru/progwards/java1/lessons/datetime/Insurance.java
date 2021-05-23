4913bb8893316f3599b252e9d09f881fa1203687
sergey <sierghieigavrilov053@gmail.com>
2021-05-23 14:48:23 +0200

    datetime

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -2,15 +2,11 @@ package ru.progwards.java1.lessons.datetime;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
 public class Insurance {
 
-    public static enum FormatStyle {SHORT, LONG, FULL}
-
     private ZonedDateTime start;
     private Duration duration;
-
     public Insurance(ZonedDateTime start) {
         this.start = start;
 
@@ -20,10 +16,10 @@ public class Insurance {
         ZoneId defaultZone = ZoneId.of("Europe/Moscow");
         DateTimeFormatter dtf;
 
-        if(FormatStyle.SHORT == style) {
+        if (FormatStyle.SHORT == style) {
             dtf = DateTimeFormatter.ISO_LOCAL_DATE;
             LocalDate ldate = LocalDate.parse(strStart, dtf);
-            LocalTime ltime = LocalTime.of(0,0,0);
+            LocalTime ltime = LocalTime.of(0, 0, 0);
             LocalDateTime temp1 = LocalDateTime.of(ldate, ltime);
             this.start = temp1.atZone(defaultZone);
 
@@ -40,7 +36,7 @@ public class Insurance {
 
     }
 
-    public void setDuration(Duration duration){
+    public void setDuration(Duration duration) {
         this.duration = duration;
     }
 
@@ -49,30 +45,27 @@ public class Insurance {
     }
 
     public void setDuration(int months, int days, int hours) {
-        duration = Duration.ofHours(hours).plusDays(days).plusDays(months* 30);
+        duration = Duration.ofHours(hours).plusDays(days).plusDays(months * 30);
     }
 
     public void setDuration(String strDuration, FormatStyle style) {
-        switch (style) {
-            case SHORT:
-                long millis = Long.parseLong(strDuration);
-                duration = Duration.ofMillis(millis);
-                break;
-            case LONG:
-                int years = Integer.parseInt(strDuration.substring(0,4));
-                int months = Integer.parseInt(strDuration.substring(5,7));
-                int days = Integer.parseInt(strDuration.substring(8,10));
-                int hours = Integer.parseInt(strDuration.substring(11,13));
-                int minutes = Integer.parseInt(strDuration.substring(14,16));
-                int seconds = Integer.parseInt(strDuration.substring(17,19));
-                duration = Duration.ofSeconds(seconds).plusMinutes(minutes).plusHours(hours);
-                duration = duration.plusDays(days).plusDays(months*30).plusDays(years*365);
-                break;
-            case FULL:
-                duration = Duration.parse(strDuration);
-                break;
-            default:
-                System.out.println("Формат задан не верно");
+
+        if (FormatStyle.SHORT == style) {
+            long millis = Long.parseLong(strDuration);
+            duration = Duration.ofMillis(millis);
+
+        } else if (FormatStyle.LONG == style) {
+            int years = Integer.parseInt(strDuration.substring(0, 4));
+            int months = Integer.parseInt(strDuration.substring(5, 7));
+            int days = Integer.parseInt(strDuration.substring(8, 10));
+            int hours = Integer.parseInt(strDuration.substring(11, 13));
+            int minutes = Integer.parseInt(strDuration.substring(14, 16));
+            int seconds = Integer.parseInt(strDuration.substring(17, 19));
+            duration = Duration.ofSeconds(seconds).plusMinutes(minutes).plusHours(hours);
+            duration = duration.plusDays(days).plusDays(months * 30).plusDays(years * 365);
+
+        } else if (FormatStyle.FULL == style) {
+            duration = Duration.parse(strDuration);
         }
     }
 
@@ -103,5 +96,7 @@ public class Insurance {
         }
         return "Insurance issued on " + start + validStr;
     }
+
+    public enum FormatStyle {SHORT, LONG, FULL}
 
 }
