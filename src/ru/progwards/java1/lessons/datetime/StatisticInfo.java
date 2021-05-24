c120c23c984b8c01eda6d34faf9c156efc216ff1
sergey <sierghieigavrilov053@gmail.com>
2021-05-24 19:49:15 +0200

    datetime

--- src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
+++ src/ru/progwards/java1/lessons/datetime/StatisticInfo.java
@@ -5,5 +5,14 @@ public class StatisticInfo {
     public String sectionName;
     public int fullTime;
     public int selfTime;
+    public int count;
+
+    public StatisticInfo (String sectionName, int fullTime, int selfTime, int count) {
+        this.sectionName = sectionName;
+        this.fullTime = fullTime;
+        this.selfTime = selfTime;
+        this.count = count;
+
+    }
 
 }
