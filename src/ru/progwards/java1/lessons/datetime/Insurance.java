210590e744a51d118e4f40bce0685281862eda55
sergey <sierghieigavrilov053@gmail.com>
2021-05-24 19:36:54 +0200

    datetime

--- src/ru/progwards/java1/lessons/datetime/Insurance.java
+++ src/ru/progwards/java1/lessons/datetime/Insurance.java
@@ -45,7 +45,7 @@ public class Insurance {
     }
 
     public void setDuration(int months, int days, int hours) {
-        duration = Duration.ofHours(hours).plusDays(days).plusDays(months * 30);
+        duration = Duration.ofHours(hours).plusDays(days).plusDays(months*30);
     }
 
     public void setDuration(String strDuration, FormatStyle style) {
@@ -55,14 +55,14 @@ public class Insurance {
             duration = Duration.ofMillis(millis);
 
         } else if (FormatStyle.LONG == style) {
-            int years = Integer.parseInt(strDuration.substring(0, 4));
-            int months = Integer.parseInt(strDuration.substring(5, 7));
-            int days = Integer.parseInt(strDuration.substring(8, 10));
-            int hours = Integer.parseInt(strDuration.substring(11, 13));
-            int minutes = Integer.parseInt(strDuration.substring(14, 16));
-            int seconds = Integer.parseInt(strDuration.substring(17, 19));
+            int years = Integer.parseInt(strDuration.substring(0,4));
+            int months = Integer.parseInt(strDuration.substring(5,7));
+            int days = Integer.parseInt(strDuration.substring(8,10));
+            int hours = Integer.parseInt(strDuration.substring(11,13));
+            int minutes = Integer.parseInt(strDuration.substring(14,16));
+            int seconds = Integer.parseInt(strDuration.substring(17,19));
             duration = Duration.ofSeconds(seconds).plusMinutes(minutes).plusHours(hours);
-            duration = duration.plusDays(days).plusDays(months * 30).plusDays(years * 365);
+            duration = duration.plusDays(days).plusDays(months*30).plusDays(years*365);
 
         } else if (FormatStyle.FULL == style) {
             duration = Duration.parse(strDuration);
--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,4 +1,38 @@
 package ru.progwards.java1.lessons.datetime;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
 public class Profiler {
+
+
+    static List<StackItem> stack = new ArrayList<>();
+    static TreeMap<String, StatisticInfo> tree = new TreeMap<>();
+
+
+    public static void enterSection(String name) {
+        stack.add(new StackItem(1,2, LocalDateTime.now()));
+    }
+
+    public static void exitSection(String name) {
+    }
+
+    public static List<StatisticInfo> getStatisticInfo() {
+        return null;
+    }
+
+    static class StackItem {
+
+        public int fullTime;// полное время выполнения секции в миллисекундах.
+        public int selfTime;// чистое время выполнения секции в миллисекундах.
+        public LocalDateTime startTime;
+
+        public StackItem(int fullTime, int selfTime, LocalDateTime startTime){
+            this.fullTime = fullTime;
+            this.selfTime = selfTime;
+            this.startTime = startTime;
+        }
+    }
 }
