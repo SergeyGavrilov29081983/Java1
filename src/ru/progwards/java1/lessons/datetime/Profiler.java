2f7f4a77b2a9ca07e326a3f655f77bb9304ed57d
sergey <sierghieigavrilov053@gmail.com>
2021-05-25 07:15:07 +0200

    datetime

--- src/ru/progwards/java1/lessons/datetime/Profiler.java
+++ src/ru/progwards/java1/lessons/datetime/Profiler.java
@@ -1,116 +1,60 @@
 package ru.progwards.java1.lessons.datetime;
 
-import java.time.LocalDateTime;
 import java.util.*;
 
 public class Profiler {
-
-    static class StackItem {
-        public int fullTime;// полное время выполнения секции в миллисекундах.
-        public int selfTime;// чистое время выполнения секции в миллисекундах.
-        public LocalDateTime startTime;
-
-        public StackItem(int i, int i1, LocalDateTime now) {
-            this.fullTime = i;
-            this.selfTime = i1;
-            this.startTime = now;
-        }
-    }
-
-    static List<StackItem> stack = new ArrayList<>();
-    static TreeMap<String, StatisticInfo> tree = new TreeMap<>();
-
+    static Map<String, StatisticInfo> Sections = new HashMap<>();
+    static Map<String, Long> startTime = new HashMap<>();
+    static Map<String, Integer> counts = new HashMap<>();
+    static Deque<String> qSections = new ArrayDeque<>();
 
     public static void enterSection(String name) {
-        {
-            stack.add(new StackItem(1,1,LocalDateTime.now()));
+        if (startTime.size() == 0 || !startTime.containsKey(name)) {
+            counts.put(name, 1);
+            qSections.addFirst(name);
+        } else {
+            counts.put(name, counts.get(name) + 1);
+            if (!name.equals(qSections.peekFirst())) {
+                qSections.addFirst(name);
+            }
         }
-        tree.put(name, new StatisticInfo(name, 1, 1, 1));
+        startTime.put(name, System.currentTimeMillis());
     }
 
     public static void exitSection(String name) {
-        {
-            stack.add(new StackItem(1,1,LocalDateTime.now()));
+        long fullTime = System.currentTimeMillis() - startTime.get(name);
+        long selfTime = fullTime;
+        long prevTime = 0;
+        boolean isNested = false;
+        while (!name.equals(qSections.peekFirst())) {
+            isNested = true;
+            String prevname = qSections.pollFirst();
+            StatisticInfo prev = Sections.get(prevname);
+            prevTime = prevTime + prev.fullTime;
         }
-        tree.put(name, new StatisticInfo(name, 1, 1, 1));
+        if (isNested) {
+            if (!Sections.isEmpty() && Sections.containsKey(name)) {
+                StatisticInfo val = Sections.get(name);
+                fullTime += val.fullTime;
+                selfTime = fullTime - prevTime;
+            } else {
+                selfTime = selfTime - prevTime;
+            }
+        } else {
+            if (!Sections.isEmpty() && Sections.containsKey(name)) {
+                StatisticInfo val = Sections.get(name);
+                fullTime += val.fullTime;
+                selfTime += val.selfTime;
+            }
+        }
+        StatisticInfo newSI = new StatisticInfo(name, (int) fullTime, (int) selfTime, counts.get(name));
+        Sections.put(name, newSI);
     }
 
     public static List<StatisticInfo> getStatisticInfo() {
-        Collection<StatisticInfo> temp = tree.values();
+        Collection<StatisticInfo> temp = Sections.values();
         List<StatisticInfo> res = new ArrayList<>(temp);
         res.sort(StatisticInfo::compareTo);
         return res;
-    }
-
-
-
-
-    public static void main(String[] args) {
-        Profiler t = new Profiler();
-
-        enterSection("Process1");
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        exitSection("Process1");
-        enterSection("Process1");
-       /* try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        exitSection("Process1");
-        enterSection("Process1");
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        exitSection("Process1");
-        enterSection("Process1");
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        enterSection("Process2");
-        try {
-            Thread.sleep(200);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        enterSection("Process3");
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        exitSection("Process3");
-        exitSection("Process2");
-        enterSection("Process2");
-        try {
-            Thread.sleep(200);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        enterSection("Process3");
-        try {
-            Thread.sleep(100);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        exitSection("Process3");
-        exitSection("Process2");
-        exitSection("Process1");*/
-
-        List<StatisticInfo> res = getStatisticInfo();
-        for (StatisticInfo tt : res) {
-            System.out.println(tt.sectionName);
-            System.out.println(tt.fullTime);
-            System.out.println(tt.selfTime);
-            System.out.println(tt.count);
-        }
     }
 }
