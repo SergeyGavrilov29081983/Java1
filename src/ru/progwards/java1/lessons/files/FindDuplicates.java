4fe13cdedc1006b65c9cf8b2359b75b1b9be7f1f
sergey <sierghieigavrilov053@gmail.com>
2021-06-16 20:24:49 +0200

    files

---
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -0,0 +1,40 @@
+package ru.progwards.java1.lessons.files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDuplicates {
+
+    private final List<List<String>> files = new ArrayList<>();
+
+    public List<List<String>> findDuplicates(String startPath) throws IOException {
+
+        Path path = Paths.get(startPath);
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**");
+
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) {
+                if (matcher.matches(path))
+                    System.out.println(path);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException e) {
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return files;
+    }
+
+
+   /* В заданном каталоге и его подкаталогах найти файлы, точно совпадающие по названию
+            (и расширению), дате-времени последнего изменения, размеру и по содержимому.
+    Сигнатура метода public List<List<String>> findDuplicates(String startPath), результат -
+    список, содержащий списки строк с именами и полными путями совпадающих файлов.
+}*/
+}
