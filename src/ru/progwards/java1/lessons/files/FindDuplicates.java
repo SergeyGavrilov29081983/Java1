d518704f4d9498634186c6a9989cd26678d399e8
sergey <sierghieigavrilov053@gmail.com>
2021-07-10 12:05:26 +0200

    files

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -4,55 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FindDuplicates {
 
     public static final List<List<String>> files = new ArrayList<>();
-    public static boolean isDuplicate = false;
 
-    public static List<List<String>> findDuplicates(String startPath) {
-        try {
-            List<Path> listPaths = Files.walk(Paths.get(startPath), 999)
-                    .filter(Files::isRegularFile)
-                    .collect(Collectors.toList());
-
-            for (Path path : listPaths) {
-                PathMatcher pm = FileSystems.getDefault().getPathMatcher("glob:**/" + path);
-                List<String> result = new ArrayList<>();
-                List<String> finalResult = result;
-                Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult visitFile(Path p, BasicFileAttributes attrs) {
-                        if (pm.matches(p)) {
-                            if (checkEquality(p.toFile(), path.toFile())) {
-                                finalResult.add(p.toString());
-                                isDuplicate = true;
-                            }
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFileFailed(Path p, IOException e) {
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-                if (!isDuplicate) {
-                    result = null;
-                } else {
-                    result.add(path.toString());
-                }
-                files.add(result);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return files;
-    }
+    public List<String> result;
 
     public static boolean checkEquality(File file1, File file2) {
         if (file1.length() == file2.length()) {
@@ -72,9 +31,44 @@ public class FindDuplicates {
     }
 
     public static void main(String[] args) {
-        ru.progwards.diffWorkClasses.FindDuplicates t = new ru.progwards.diffWorkClasses.FindDuplicates();
+        FindDuplicates t = new FindDuplicates();
         List<List<String>> tt = t.findDuplicates("outFiles");
         for (List<String> l : tt)
             System.out.println(l);
+    }
+
+    public List<List<String>> findDuplicates(String startPath) {
+        try {
+            List<Path> listPaths = Files.walk(Paths.get(startPath), 999)
+                    .filter(Files::isRegularFile)
+                    .collect(Collectors.toList());
+
+            for (Path path : listPaths) {
+                PathMatcher pm = FileSystems.getDefault().getPathMatcher("glob:**/" + path);
+                result = new ArrayList<>();
+                Files.walkFileTree(Paths.get(startPath), new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path p, BasicFileAttributes attrs) {
+                            if (checkEquality(p.toFile(), path.toFile())) {
+                                result.add(p.toString());
+                            }
+                        return FileVisitResult.CONTINUE;
+                    }
+
+                    @Override
+                    public FileVisitResult visitFileFailed(Path p, IOException e) {
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+                files.add(result);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        files.removeIf(file -> file.size() == 1);
+        Set<List<String>> set = new HashSet<>(files);
+        files.clear();
+        files.addAll(set);
+        return files;
     }
 }
